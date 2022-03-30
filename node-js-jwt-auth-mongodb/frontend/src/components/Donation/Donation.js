4a5ab6b2bbf52e1ee35a99397adf239e91c48b61
Amrut Sagar <amrutsagar06@gmail.com>
2022-03-30 23:29:19 +0530

    donation edited

--- node-js-jwt-auth-mongodb/frontend/src/components/Donation/Donation.js
+++ node-js-jwt-auth-mongodb/frontend/src/components/Donation/Donation.js
@@ -1,173 +1,33 @@
-// import "./Donation.css";
-// import axios from "axios";
-// import React, { useEffect, useState } from "react";
-// // import Header from "../Header/Header";
-// import Header from "../Header";
-
-// function App() {
-//   const [loading, setLoading] = useState(false);
-//   const [orderAmount, setOrderAmount] = useState(0);
-//   const [orders, setOrders] = useState([]);
-
-//   async function fetchOrders() {
-//     const { data } = await axios.get("/list-orders");
-//     setOrders(data);
-//   }
-//   useEffect(() => {
-//     fetchOrders();
-//   }, []);
-
-//   function loadRazorpay() {
-//     const script = document.createElement("script");
-//     script.src = "https://checkout.razorpay.com/v1/checkout.js";
-//     script.onerror = () => {
-//       alert("Razorpay SDK failed to load. Are you online?");
-//     };
-//     script.onload = async () => {
-//       try {
-//         setLoading(true);
-//         const result = await axios.post("/create-order", {
-//           amount: orderAmount + "00",
-//         });
-//         const { amount, id: order_id, currency } = result.data;
-//         const {
-//           data: { key: razorpayKey },
-//         } = await axios.get("/get-razorpay-key");
-
-//         const options = {
-//           key: razorpayKey,
-//           amount: amount.toString(),
-//           currency: currency,
-//           name: "example name",
-//           description: "example transaction",
-//           order_id: order_id,
-//           handler: async function (response) {
-//             const result = await axios.post("/pay-order", {
-//               amount: amount,
-//               razorpayPaymentId: response.razorpay_payment_id,
-//               razorpayOrderId: response.razorpay_order_id,
-//               razorpaySignature: response.razorpay_signature,
-//             });
-//             alert(result.data.msg);
-//             fetchOrders();
-//           },
-//           prefill: {
-//             name: "example name",
-//             email: "email@example.com",
-//             contact: "111111",
-//           },
-//           notes: {
-//             address: "example address",
-//           },
-//           theme: {
-//             color: "#80c0f0",
-//           },
-//         };
-
-//         setLoading(false);
-//         const paymentObject = new window.Razorpay(options);
-//         paymentObject.open();
-//       } catch (err) {
-//         alert(err);
-//         setLoading(false);
-//       }
-//     };
-//     document.body.appendChild(script);
-//   }
-
-//   return (
-//     <div className="Header">
-//       <Header />
-//       <hr />
-//       <div className="App">
-//         <div>
-//           <h1 className="donate-header">Donate to feed people in hunger</h1>
-//         </div>
-//         <div>
-//           <div id="form-view" className="donate-heading">
-//             <form className="form">
-//               <div>
-//                 <div className="form">
-//                   Email <span style={{ text: "secondary" }}>(optional)</span>
-//                 </div>
-//                 <input placeholder="email"></input>
-//               </div>
-//               <div className="form">
-//                 Phone
-//                 <span>(optional)</span>
-//               </div>
-
-//               <div>
-//                 <input placeholder="number"></input>
-//               </div>
-//               <div className="form">Amount</div>
-//               <div>
-//                 <input
-//                   placeholder="INR"
-//                   type="number"
-//                   value={orderAmount}
-//                   onChange={(e) => setOrderAmount(e.target.value)}
-//                 ></input>
-//               </div>
-//             </form>
-
-//             <button className="form" disabled={loading} onClick={loadRazorpay}>
-//               Donate
-//             </button>
-//             {loading && <div>Loading...</div>}
-//           </div>
-//         </div>
-//       </div>
-//       <div className="donation-details">
-//         <h2 className="details-screen" style={{ color: "green" }}>
-//           Donate For Cause
-//         </h2>
-//         <h4 className="food-wate">
-//           Did you know that about 40 % of the food produced in India is wasted?
-//         </h4>
-//         <h4 className="food-waste-money">
-//           Despite adequate food production, the UN has reported that about 190
-//           million Indians remain undernourished. It is further estimated that
-//           the value of food wastage in India is around ₹92,000 crores per annum.
-//         </h4>
-//         <h4>
-//           The world recognises that no truly sustainable and developed country
-//           can exist without tackling the issue of food waste, and while the
-//           challenge in front of us may be monumental, some steps need to be
-//           taken sooner than others.
-//         </h4>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import "./Donation.css";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+// import Header from "../Header/Header";
 import Header from "../Header";
 import VideoPlayer from "react-video-js-player";
 import foodwaste from "./foodwaste.mp4";
 import emailjs from "emailjs-com";
-
-const poster =
-  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.M3rZ1mR7ijK00nzPr1XruQHaD4%26pid%3DApi&f=1";
+import image2 from "./food_loss_food_waste.jpg";
+import image1 from "./food_waste_graph.jpg";
+// import image2 from "./food loss food waste.JPG";
+import image3 from "./food_waste_recycle.jpg";
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [orderAmount, setOrderAmount] = useState(0);
-  const [orders, setOrders] = useState([]);
+  // const [orders, setOrders] = useState([]);
 
-  async function fetchOrders() {
-    const { data } = await axios.get("/list-orders");
-    setOrders(data);
-  }
+  //fetching orders by connecting to backend
+  // async function fetchOrders() {
+  //   const { data } = await axios.get("/list-orders");
+  //   setOrders(data);
+  // }
 
-  useEffect(() => {
-    fetchOrders();
-  }, []);
+  // to show orders list in frontend
+  // useEffect(() => {
+  //   fetchOrders();
+  // }, []);
 
+  //to send Email to users after donating using EmailJs
   function sendEmail(e) {
     e.preventDefault();
     return emailjs
@@ -185,16 +45,18 @@ function App() {
       });
   }
 
+  //
   function loadRazorpay() {
-    const script = document.createElement("script");
-    script.src = "https://checkout.razorpay.com/v1/checkout.js";
+    const script = document.createElement("script"); //creating script element
+    script.src = "https://checkout.razorpay.com/v1/checkout.js"; //source coming from razorpay
     script.onerror = () => {
-      alert("Razorpay SDK failed to load. Are you online?");
+      alert("Razorpay SDK failed to load. Are you online?"); //show error if not loaded or any trouble in loading script
     };
     script.onload = async () => {
       try {
-        setLoading(true);
+        setLoading(true); //loading is true then only ceates
         const result = await axios.post("/create-order", {
+          //creating order which passes amount to api coming from frontend
           amount: orderAmount + "00",
         });
         const { amount, id: order_id, currency } = result.data;
@@ -210,19 +72,21 @@ function App() {
           description: "example transaction",
           order_id: order_id,
           handler: async function (response) {
+            //when the payment is successful passes the data to api
             const result = await axios.post("/pay-order", {
               amount: amount,
               razorpayPaymentId: response.razorpay_payment_id,
               razorpayOrderId: response.razorpay_order_id,
               razorpaySignature: response.razorpay_signature,
             });
-            alert(result.data.msg);
-            fetchOrders();
+            alert(result.data.msg); //alert to user that payment is successful
+            // fetchOrders();                //to show new payments in the frontend
           },
           prefill: {
-            name: "example name",
+            //prefill section to set dummy details in razorpay window
+            name: " name",
             email: "email@example.com",
-            contact: "111111",
+            contact: "1111111111",
           },
           notes: {
             address: "example address",
@@ -233,14 +97,14 @@ function App() {
         };
 
         setLoading(false);
-        const paymentObject = new window.Razorpay(options);
-        paymentObject.open();
+        const paymentObject = new window.Razorpay(options); //goes to new window of razorpay
+        paymentObject.open(); // and opens
       } catch (err) {
         alert(err);
         setLoading(false);
       }
     };
-    document.body.appendChild(script);
+    document.body.appendChild(script); // adding script after onload
   }
 
   return (
@@ -257,10 +121,18 @@ function App() {
               <form className="form">
                 <div>
                   <div className="form">Email</div>
-                  <input type="email" placeholder="email"></input>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    required="true"
+                  ></input>
                 </div>
                 <div className="form">Username</div>
-                <input type="text" placeholder="user_name"></input>
+                <input
+                  type="text"
+                  placeholder="user_name"
+                  required="true"
+                ></input>
 
                 <div className="form">
                   Phone
@@ -279,7 +151,7 @@ function App() {
               </form>
 
               <button
-                style={{ color: "green" }}
+                style={{ backgroundColor: "Highlight" }}
                 disabled={loading}
                 onClick={() => {
                   loadRazorpay();
@@ -326,10 +198,52 @@ function App() {
             sustainable India that does not have millions undernourished despite
             having adequate food production.
           </h4>
-          <div className="video">
-            <VideoPlayer src={foodwaste} poster={poster}></VideoPlayer>
-          </div>
         </div>
+      </div>
+      <div images>
+        <img className="images-statistics" src={image1} alt="">
+          {/* <h2> How much food we waste</h2> */}
+        </img>
+        <img src={image2} alt=""></img>
+        <img
+          src={image3}
+          alt=""
+          style={{ width: "500px", height: "430px" }}
+        ></img>
+      </div>
+      <li className="data-satistics">
+        33% of food is wasting in the available food for us.Around one-third of
+        the world’s food is lost to waste or 1.3 billion tons per year.
+      </li>
+      <li className="data-satistics">
+        Saving even just a fourth of the total global food waste volume can feed
+        all the world’s hungry.
+      </li>
+      <li className="data-satistics">
+        Mostly the food waste is happening in storing the raw food.{" "}
+      </li>
+      <li className="data-satistics">
+        As we can see in the graph 72% food loss is happening before it is
+        reaching to anyone.
+      </li>
+      <li className="data-satistics">
+        If we can follow the food waste cycle that is shown in the picture we
+        can save food that feed atleast 70% of the people who are suffering lack
+        of food.
+      </li>
+      <li className="data-satistics">
+        Hence we encourage all people to not waste food and keep donating food.
+      </li>
+
+      <div>
+        <VideoPlayer
+          className="video"
+          src={foodwaste}
+          poster="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.e0yhGmjZ3CvTojnjpsfg6QHaE6%26pid%3DApi&f=1"
+        ></VideoPlayer>
+        <h3 className="videotag">
+          A small Video to explain food wastage and its impacts
+        </h3>
       </div>
     </div>
   );
